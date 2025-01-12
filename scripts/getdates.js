1b420f791fe471aa1c0a0f1b710ef6f961c2b40f
caleb-beardall <calebbeardall@byui.edu>
2025-01-12 11:00:52 -0800

    Completed W01 Assignment: Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+const year = document.querySelector("#year");
+
+const today = new Date();
+
+year.innerHTML = `©<span class="highlight">${today.getFullYear()}</span> | Caleb Beardall | Ogden, Utah`;
+
+let modDate = document.querySelector("#modified-date");
+
+let oLastModif = new Date(document.lastModified);
+
+modDate.innerHTML = `Last Modification: <span class="highlight">${oLastModif.toLocaleString('de-DE')}</span>`;
